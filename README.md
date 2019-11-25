e154dc8eb4f4573582b4a4556dc944887f9a453b
marmai-jma <johane.marmai@gmail.com>
2019-11-25 12:25:49 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Fichiers pour la formation "Révisions JavaScript"
+
+Fichiers de démarrage pour les exercices de la formation "Révisions JavaScript".
---
+++ ts/todo.ts
@@ -0,0 +1,14 @@
+(function () {
+  //récupère une référence à l'élement
+  var appElement;
+
+ //attend que la page soit complétement chargée pour démarer l'application. 
+  window.addEventListener('load',startApp);  
+
+ function startApp(){
+  appElement = document.getElementById('app');
+  //change de texte de la balise
+  appElement.textContent=""
+ }
+
+})();
